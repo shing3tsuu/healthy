00b06fee1f00aa9dcb5805381a48185fe73b2a05
shing3tsuu <154595182+shing3tsuu@users.noreply.github.com>
2025-05-26 18:05:44 +0400

    Update setup.py

--- healthy/services/telegram/setup.py
+++ healthy/services/telegram/setup.py
@@ -43,12 +43,22 @@ class DialogSetup:
             ),
             Window(
                 Format("🌍 Твой часовой пояс:"),
-                Button(
-                    Const("+1"),
-                    id="tz_plus_1",
-                    on_click=partial(self.handlers.on_confirm_timezone, offset=1)
+                Group(
+                    Button(Const("+0"), id="tz_plus_0", on_click=partial(self.handlers.on_confirm_timezone, offset=0)),
+                    Button(Const("+1"), id="tz_plus_1", on_click=partial(self.handlers.on_confirm_timezone, offset=1)),
+                    Button(Const("+2"), id="tz_plus_2", on_click=partial(self.handlers.on_confirm_timezone, offset=2)),
+                    Button(Const("+3"), id="tz_plus_3", on_click=partial(self.handlers.on_confirm_timezone, offset=3)),
+                    Button(Const("+4"), id="tz_plus_4", on_click=partial(self.handlers.on_confirm_timezone, offset=4)),
+                    Button(Const("+5"), id="tz_plus_5", on_click=partial(self.handlers.on_confirm_timezone, offset=5)),
+                    Button(Const("+6"), id="tz_plus_6", on_click=partial(self.handlers.on_confirm_timezone, offset=6)),
+                    Button(Const("+7"), id="tz_plus_7", on_click=partial(self.handlers.on_confirm_timezone, offset=7)),
+                    Button(Const("+8"), id="tz_plus_8", on_click=partial(self.handlers.on_confirm_timezone, offset=8)),
+                    width=3,
                 ),
                 Cancel(Const("❌ Отмена")),
                 state=DialogSG.CONFIRM_TIMEZONE
-            )
+            ),
+            # Window(
+            #     Format("Главное меню, здесь ты можешь настраивать свои привычки, читать подсказки и устанавливать напоминания"),
+            #  )
         )
