4738d3e0f893d5160714f1f177558ac80def0266
shing3tsuu <154595182+shing3tsuu@users.noreply.github.com>
2025-06-29 01:21:33 +0400

    Update user_dao.py

--- healthy/core/dao/user_dao.py
+++ healthy/core/dao/user_dao.py
@@ -3,25 +3,23 @@ from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from datetime import datetime, date, time
 
-from core.database.dto import UserDomain, UserHabitDomain, ReminderDomain, RelapseHistoryDomain
+from core.database.dto import *
 
 class BaseUserGateway(ABC):
     @abstractmethod
-    async def create_user(self, tg_id: int, username: Optional[str], first_name: Optional[str], timezone: str) -> UserDomain:
+    async def create_user(
+            self,
+            tg_id: int,
+            username: Optional[str],
+            first_name: Optional[str],
+            timezone: str
+    ) -> UserDomain:
+        """
+        Create a new user in the database.
+        by tg_id: Telegram ID of the user.
+        by username: Telegram profile name of the user.
+        by first_name: TextInput name of the user.
+        by timezone: pytz.timezone of the user.
+        :return UserDomain pydantic model of the user
+        """
         raise NotImplementedError()
-    # Создание пользователя в БД
-
-    @abstractmethod
-    async def get_user_by_tg_id(self, tg_id: int) -> UserDomain:
-        raise NotImplementedError()
-    # Получение пользователя по его Telegram ID
-
-    @abstractmethod
-    async def create_user_habit(self, user_id: int, habit_id: int, start_date: datetime) -> UserHabitDomain:
-        raise NotImplementedError()
-    # Создание привычки пользователя
-
-    @abstractmethod
-    async def get_user_habit_by_id(self, user_id: int) -> UserHabitDomain:
-        raise NotImplementedError()
-    # Получение привычки пользователя по его User ID
