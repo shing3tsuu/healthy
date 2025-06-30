b691475b9f46a855571595e63db3832db25e4085
shing3tsuu <154595182+shing3tsuu@users.noreply.github.com>
2025-06-30 19:48:05 +0400

    Update config_reader.py

--- healthy/config_reader.py
+++ healthy/config_reader.py
@@ -1,21 +1,28 @@
 from dataclasses import dataclass
 from environs import Env
+from typing import Optional
 
 @dataclass
 class TgBot:
+    """ Telegram """
     token: str
     admin_ids: list[int]
 
 @dataclass
 class DBConfig:
-    host: str
-    port: int
-    name: str
-    user: str
-    password: str
+    """ PostgreSQL """
+    host: Optional[str] = None
+    port: Optional[int] = None
+    name: Optional[str] = None
+    user: Optional[str] = None
+    password: Optional[str] = None
+
+    """ SQLite """
+    path: Optional[str] = None
 
 @dataclass
 class Config:
+    """ Config """
     tg_bot: TgBot
     db: DBConfig
 
@@ -24,15 +31,16 @@ def load_config(path: str | None) -> Config:
     env.read_env(path)
 
     return Config(
-    tg_bot=TgBot(
-        token=env('BOT_TOKEN'),
-        admin_ids=list(map(int, env.list('ADMIN_IDS')))
-    ),
-    db=DBConfig(
-        host=env('DB_HOST'),
-        port=env('DB_PORT'),
-        name=env('DB_NAME'),
-        user=env('DB_USER'),
-        password=env('DB_PASSWORD')
+        tg_bot=TgBot(
+            token=env('BOT_TOKEN'),
+            admin_ids=list(map(int, env.list('ADMIN_IDS')))
+        ),
+        db=DBConfig(
+            host=env('DB_HOST', None),
+            port=env.int('DB_PORT', None),
+            name=env('DB_NAME', None),
+            user=env('DB_USER', None),
+            password=env('DB_PASSWORD', None),
+            path=env('DB_PATH', 'data/bot.db')
+        )
     )
-)
