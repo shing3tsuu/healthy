053e2ab378fb18a45e5a3461c08afc1b6c00687f
shing3tsuu <154595182+shing3tsuu@users.noreply.github.com>
2025-06-30 19:47:18 +0400

    Update states.py

--- healthy/services/telegram/states.py
+++ healthy/services/telegram/states.py
@@ -1,6 +1,28 @@
 from aiogram.fsm.state import StatesGroup, State
 
 class DialogSG(StatesGroup):
-    MAIN = State()
+    """ADMIN"""
+    ADMIN = State()
+    ADMIN_HABIT_MANAGE = State()
+    ADMIN_ADD_HABIT = State()
+
+    ADMIN_EDIT_HABIT = State()
+    ADMIN_EDIT_HABIT_NAME = State()
+    ADMIN_EDIT_HABIT_COST = State()
+
+    ADMIN_ADD_INFO = State()
+    ADMIN_ADD_HINT = State()
+    ADMIN_ADD_HABIT_COST = State()
+
+    """INTRO"""
+    INTRO = State()
     ADD_NAME = State()
     CONFIRM_TIMEZONE = State()
+
+    """MAIN MENU"""
+    MAIN_MENU = State()
+    USER_HABITS = State()
+    ADD_HABIT = State()
+    HABIT_DETAIL = State()
+    HABIT_STATS = State()
+    RELAPSE_TRACKING = State()
