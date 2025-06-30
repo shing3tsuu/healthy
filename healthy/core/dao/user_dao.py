adc1134b7e6b2fa6928f5ccd729a2510b712492b
shing3tsuu <154595182+shing3tsuu@users.noreply.github.com>
2025-06-30 19:50:36 +0400

    Update user_dao.py

--- healthy/core/dao/user_dao.py
+++ healthy/core/dao/user_dao.py
@@ -1,4 +1,30 @@
-@abstractmethod
+from typing import Awaitable, Optional
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from datetime import datetime, date, time
+
+from core.database.dto import *
+
+class BaseUserGateway(ABC):
+    @abstractmethod
+    async def create_user(
+            self,
+            tg_id: int,
+            username: Optional[str],
+            first_name: Optional[str],
+            timezone: str
+    ) -> UserDomain:
+        """
+        Create a new user in the database.
+        tg_id: Telegram ID of the user.
+        username: Telegram profile name of the user.
+        first_name: TextInput name of the user.
+        timezone: pytz.timezone of the user.
+        :return UserDomain model of the user
+        """
+        raise NotImplementedError()
+
+    @abstractmethod
     async def add_admin_habit(
             self,
             name: str,
