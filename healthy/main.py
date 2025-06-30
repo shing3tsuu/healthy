52d62d10653bb1850d1d2940e88dc08db7d59da3
shing3tsuu <154595182+shing3tsuu@users.noreply.github.com>
2025-06-30 19:48:23 +0400

    Update main.py

--- healthy/main.py
+++ healthy/main.py
@@ -1,11 +1,3 @@
-import asyncio
-import logging
-from contextlib import asynccontextmanager
-from typing import Any, Awaitable, Callable, Dict, AsyncGenerator
-import winloop
-
-import sqlalchemy as db
-from aiogram import Bot, Dispatcher, types, BaseMiddleware, Router
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.types import Message, TelegramObject
@@ -14,7 +6,6 @@ from sqlalchemy.ext.asyncio import (
     async_sessionmaker,
     create_async_engine,
 )
-from core.database.database import *
 from services.telegram.handlers import *
 from services.telegram.telegram import *
 from config_reader import load_config
@@ -28,13 +19,10 @@ logger = logging.getLogger(__name__)
 
 
 async def main():
-    try:
-        config = load_config(".env")
-        tg_app = TelegramApp(config)
-        await tg_app.run()
-    except Exception as e:
-        logger.exception("Application failed to start: %s", e)
-
+    config = load_config(".env")
+    tg_app = TelegramApp(config)
+    await tg_app.setup()  # Теперь setup асинхронный
+    await tg_app.run()
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
