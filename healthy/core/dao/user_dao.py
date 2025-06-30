94baff86339cf0846a7ebc1718c30a869f62ec29
shing3tsuu <154595182+shing3tsuu@users.noreply.github.com>
2025-06-30 19:42:55 +0400

    Update user_dao.py

--- healthy/core/dao/user_dao.py
+++ healthy/core/dao/user_dao.py
@@ -1,25 +1,29 @@
-from typing import Awaitable, Optional
-from abc import ABC, abstractmethod
-from dataclasses import dataclass
-from datetime import datetime, date, time
-
-from core.database.dto import *
-
-class BaseUserGateway(ABC):
-    @abstractmethod
-    async def create_user(
+@abstractmethod
+    async def add_admin_habit(
             self,
-            tg_id: int,
-            username: Optional[str],
-            first_name: Optional[str],
-            timezone: str
-    ) -> UserDomain:
+            name: str,
+            cost_per_unit: Optional[int],
+    ) -> HabitDomain:
         """
-        Create a new user in the database.
-        by tg_id: Telegram ID of the user.
-        by username: Telegram profile name of the user.
-        by first_name: TextInput name of the user.
-        by timezone: pytz.timezone of the user.
-        :return UserDomain pydantic model of the user
+        Create a new habit in database by admin.
+        name: name of the habit.
+        cost_per_unit: cost per unit (day) of the habit.
+        :return HabitDomain model of the habit
+        """
+        raise NotImplementedError()
+
+    @abstractmethod
+    async def add_admin_hint(
+            self,
+            name: str,
+            description: str,
+            habit_id: int
+    ) -> HintDomain:
+        """
+        Create a new hint to habit in database by admin.
+        name: name of the hint.
+        description: description of the hint.
+        habit_id: habit id of the hint.
+        :return HintDomain model of the habit
         """
         raise NotImplementedError()
